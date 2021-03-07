ed4a1bb9e6c5383921c5e4285d0cca7c4212a740
Oliver Liermann <oliver.liermann@gmail.com>
2021-03-07 15:15:50 +0100

    version bump, only load certain functions of lodash, update deps

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'use strict'
 
-const _ = require('lodash')
+const _random = require('lodash/random')
+const _shuffle = require('lodash/shuffle')
+const _map = require('lodash/map')
+const _padStart = require('lodash/padStart')
+const _parseInt = require('lodash/parseInt')
+
 
 module.exports = Tokey
 // create an instance with a specified key, otherwise a random key will be generated
@@ -12,7 +17,8 @@ function Tokey(key) {
 }
 
 // creates a random key based on 'options'
-Tokey.prototype.createKey = function createKey(options = {}) {
+Tokey.prototype.createKey = function createKey(options) {
+    options = options || {}
     let self = this
     let key = {}
 
@@ -22,7 +28,7 @@ Tokey.prototype.createKey = function createKey(options = {}) {
     // 0=48, 9=57, A=65, Z=90, a=97, z=122
     key.contentOffset =
         options.contentOffset ||
-        (!!_.random(0, 1) ? _.random(17, 33) : _.random(49, 65))
+        (!!_random(0, 1) ? _random(17, 33) : _random(49, 65))
 
     key.randomStringLength = options.randomStringLength || 26
 
@@ -30,13 +36,14 @@ Tokey.prototype.createKey = function createKey(options = {}) {
     for (let i = 0; i < self.dateStringLength + key.randomStringLength; i++) {
         key.mix.push(i)
     }
-    key.mix = _.shuffle(key.mix)
+    key.mix = _shuffle(key.mix)
 
     self.key = key
     return key
 }
 
-Tokey.prototype.generate = function (options = {}) {
+Tokey.prototype.generate = function (options) {
+    options = options || {}
     let self = this
 
     let date = options.date || new Date()
@@ -49,10 +56,10 @@ Tokey.prototype.generate = function (options = {}) {
         minutes: date.getUTCMinutes(),
         seconds: date.getUTCSeconds(),
     }
-    let dateString = _.map(dateParts, (p) => _.padStart(p, 2, '0')).join('')
+    let dateString = _map(dateParts, (p) => _padStart(p, 2, '0')).join('')
 
     // offset content
-    dateString = _.map(dateString, (s) =>
+    dateString = _map(dateString, (s) =>
         String.fromCharCode(s.charCodeAt(0) + self.key.contentOffset),
     ).join('')
 
@@ -61,14 +68,14 @@ Tokey.prototype.generate = function (options = {}) {
         '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     for (let i = 0; i < self.key.randomStringLength; i++) {
         randomString += randomStringSource.substr(
-            _.random(0, randomStringSource.length - 1),
+            _random(0, randomStringSource.length - 1),
             1,
         )
     }
 
     let combinedString = dateString + randomString
 
-    let mixed = _.map(self.key.mix, (pos) => combinedString[pos]).join('')
+    let mixed = _map(self.key.mix, (pos) => combinedString[pos]).join('')
 
     return mixed
 }
@@ -83,7 +90,7 @@ Tokey.prototype.verify = function (token) {
         return false
     }
 
-    let unmixed = _.map(
+    let unmixed = _map(
         self.key.mix,
         (pos, index) => token[self.key.mix.indexOf(index)],
     ).join('')
@@ -93,18 +100,18 @@ Tokey.prototype.verify = function (token) {
         unmixed.length - self.key.randomStringLength,
     )
 
-    dateString = _.map(dateString, (s) =>
+    dateString = _map(dateString, (s) =>
         String.fromCharCode(s.charCodeAt(0) - self.key.contentOffset),
     ).join('')
 
     let date = new Date(
         Date.UTC(
-            _.parseInt(dateString.substr(0, 4)),
-            _.parseInt(dateString.substr(4, 2)) - 1,
-            _.parseInt(dateString.substr(6, 2)),
-            _.parseInt(dateString.substr(8, 2)),
-            _.parseInt(dateString.substr(10, 2)),
-            _.parseInt(dateString.substr(12, 2)),
+            _parseInt(dateString.substr(0, 4)),
+            _parseInt(dateString.substr(4, 2)) - 1,
+            _parseInt(dateString.substr(6, 2)),
+            _parseInt(dateString.substr(8, 2)),
+            _parseInt(dateString.substr(10, 2)),
+            _parseInt(dateString.substr(12, 2)),
         ),
     )
 
